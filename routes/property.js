d0ed0f8fff2b4f0a78142f018a06abe1a8f214e4
Nayem <nayemk900@gmail.com>
2021-08-10 17:16:05 +0600

    new api create

--- routes/property.js
+++ routes/property.js
@@ -1,30 +1,44 @@
 const router = require("express").Router();
 const Property = require("../models/PostData");
+const ObjectId = require('mongodb').ObjectID;
+
 
 router.post("/add-post", async (req, res) => {
-    const newProperty = req.body
-    
-    try {
-      // Create new user
-      const newPost = await new Property(newProperty);
-  
-      //   save and return response
-      const property = await newPost.save();
-      res.status(200).json(property);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  const newProperty = req.body
+
+  try {
+    // Create new user
+    const newPost = await new Property(newProperty);
+
+    //   save and return response
+    const property = await newPost.save();
+    res.status(200).json(property);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/all-post", async (req, res) => {
-    try {
-      Property.find({})
-        .then((result) => {
-            res.send(result)
-        })
-    } catch (err) {
-        console.log(error)
-      res.status(500).json(err);
-    }
-  });
-  module.exports = router;
+  try {
+    Property.find({})
+      .then((result) => {
+        res.send(result)
+      })
+  } catch (err) {
+    console.log(error)
+    res.status(500).json(err);
+  }
+});
+
+router.get("/single-property/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const singleProperty = await Property.findById(id)
+    !singleProperty && res.status(400).json("No Post found");
+    res.status(200).json(singleProperty);
+  } catch (err) {
+    console.log(error)
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
